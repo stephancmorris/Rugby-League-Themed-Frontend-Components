ce157d76b9ebecbe6dcd18717920dc885831d7d1
Stephan Morris <stephancmorris@gmail.com>
2025-11-13 21:48:12 +1100

    Initialize Project

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'system-ui, sans-serif' }}>
+      <h1>NRL Component Library</h1>
+      <p>Component development starting soon...</p>
+    </div>
+  );
+}
+
+export default App;
